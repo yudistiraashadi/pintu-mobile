e0097872eb311b1e0d01166f09fb6b398584f3ec
Yudistira Ashadi <distra96@yahoo.com>
2023-03-08 21:33:32 +0700

    finished market screen

--- app/screens/MarketScreen.tsx
+++ app/screens/MarketScreen.tsx
@@ -1,83 +1,132 @@
 import { observer } from "mobx-react-lite"
 import React, { FC } from "react"
-import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
-import { useStores } from "../models"
+import { ViewStyle } from "react-native"
+import { Screen, Header } from "../components"
 import { AppStackScreenProps } from "../navigators"
-import { colors, spacing } from "../theme"
-import { useHeader } from "../utils/useHeader"
-import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
 
-import { Text, Button } from "native-base"
-
-const welcomeLogo = require("../../assets/images/logo.png")
-const welcomeFace = require("../../assets/images/welcome-face.png")
+import { Box, Text } from "native-base"
+import { useQuery, useQueryClient } from "react-query"
+import axios from "axios"
 
 interface MarketScreenProps extends AppStackScreenProps<"Market"> {}
 
+/**
+ * Main Function
+ */
 export const MarketScreen: FC<MarketScreenProps> = observer(function MarketScreen() {
-  useHeader({
-    rightText: "Search",
-    onRightPress: () => {},
+  const queryClient = useQueryClient()
+
+  const pricesQuery = useQuery<any, Error>(
+    "prices",
+    async () => {
+      const res = await axios.get("https://api.pintu.co.id/v2/trade/price-changes")
+      return res.data.payload
+    },
+    {
+      refetchInterval: 2000,
+    },
+  )
+
+  const currencyQuery = useQuery<any, Error>("currencies", async () => {
+    const res = await axios.get("https://api.pintu.co.id/v2/wallet/supportedCurrencies")
+    return res.data.payload
   })
 
-  const $bottomContainerInsets = useSafeAreaInsetsStyle(["bottom"])
+  /**
+   * Render header
+   */
+  const _renderHeader = () => <Header backgroundColor="white" title={"Market"} />
 
+  const _renderListItem = ({ currency, price }) => {
+    return (
+      <Box
+        key={currency.currencyGroup}
+        w={"100%"}
+        paddingY={2}
+        paddingX={4}
+        borderBottomWidth={0.25}
+        borderBottomColor={"#333"}
+      >
+        <Box flexDir={"row"} alignItems={"center"}>
+          {/* icon */}
+          <Box
+            width={12}
+            height={12}
+            borderRadius={"full"}
+            bgColor={currency.color}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Text color={"white"}>{currency.currencySymbol}</Text>
+          </Box>
+
+          {/* name */}
+          <Box flex={1} paddingX={2}>
+            <Text fontWeight={"bold"}>{currency.name}</Text>
+
+            <Text>{currency.currencyGroup}</Text>
+          </Box>
+
+          {/* price */}
+          <Box flex={1} paddingX={2} justifyContent={"flex-end"} alignItems={"flex-end"}>
+            <Text fontWeight={"bold"}>
+              Rp {Intl.NumberFormat("id-ID").format(price.latestPrice)}
+            </Text>
+
+            <Text>{price.day} %</Text>
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+
+  const _renderList = () => {
+    if (pricesQuery.isLoading || currencyQuery.isLoading) return <Text>Loading...</Text>
+
+    if (pricesQuery.isError) return <Text>Error: {pricesQuery.error.message}</Text>
+
+    if (currencyQuery.isError) return <Text>Error: {currencyQuery.error.message}</Text>
+
+    return (
+      <Box>
+        {currencyQuery.data
+          .filter((i) => i.currencyGroup !== "IDR")
+          .map((currency) => {
+            let pricePair = currency.currencyGroup.toLowerCase() + "/idr"
+            let price = pricesQuery.data.find((item) => item.pair == pricePair)
+
+            if (price) {
+              let itemData = {
+                currency,
+                price,
+              }
+
+              return _renderListItem(itemData)
+            }
+
+            return null
+          })}
+      </Box>
+    )
+  }
+
+  /**
+   * Main Return
+   */
   return (
-    <View style={$container}>
-      <View style={$topContainer}>
-        <Image style={$welcomeLogo} source={welcomeLogo} resizeMode="contain" />
-        <Text testID="welcome-heading" style={$welcomeHeading}>
-          Ready for Launch!
-        </Text>
-        <Text>Exciting</Text>
-        <Image style={$welcomeFace} source={welcomeFace} resizeMode="contain" />
-      </View>
+    <>
+      {/* Header */}
+      {_renderHeader()}
 
-      <View style={[$bottomContainer, $bottomContainerInsets]}>
-        <Text size="md">Something</Text>
-        <Button testID="next-screen-button">BUTTON</Button>
-      </View>
-    </View>
+      <Screen preset={"scroll"} style={$screen}>
+        {/* Content */}
+        {_renderList()}
+      </Screen>
+    </>
   )
 })
 
-const $container: ViewStyle = {
+const $screen: ViewStyle = {
   flex: 1,
-  backgroundColor: colors.background,
-}
-
-const $topContainer: ViewStyle = {
-  flexShrink: 1,
-  flexGrow: 1,
-  flexBasis: "57%",
-  justifyContent: "center",
-  paddingHorizontal: spacing.large,
-}
-
-const $bottomContainer: ViewStyle = {
-  flexShrink: 1,
-  flexGrow: 0,
-  flexBasis: "43%",
-  backgroundColor: colors.palette.neutral100,
-  borderTopLeftRadius: 16,
-  borderTopRightRadius: 16,
-  paddingHorizontal: spacing.large,
-  justifyContent: "space-around",
-}
-const $welcomeLogo: ImageStyle = {
-  height: 88,
-  width: "100%",
-  marginBottom: spacing.huge,
-}
-
-const $welcomeFace: ImageStyle = {
-  height: 169,
-  width: 269,
-  position: "absolute",
-  bottom: -47,
-  right: -80,
-}
-
-const $welcomeHeading: TextStyle = {
-  marginBottom: spacing.medium,
+  backgroundColor: "white",
 }
